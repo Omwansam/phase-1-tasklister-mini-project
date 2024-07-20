ecaf20e7975f26ab735ef59de58275112cc6c5ff
Omwansam <omwansamarnold@gmail.com>
2024-07-20 19:37:13 +0300

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', function(){
+
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event){
+      event.preventDefault();
+
+      //gets value inputed in form
+      const taskDescription = document.getElementById('new-task-description').value;
+
+      //creates a new list item
+      const newTask = document.createElement('li');
+      newTask.textContent = taskDescription;
+
+      //append the new task to the task list
+      const taskList = document.getElementById('tasks');
+      taskList.appendChild(newTask);
+
+
+   // Clear the input field
+   document.getElementById('new-task-description').value = '';
+  
+
+  });
+  
 });
